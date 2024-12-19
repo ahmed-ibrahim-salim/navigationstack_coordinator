8746ffb6b9a877008c54db044aa8e8039fad80ff
Ahmed Ibrahim <ahmed.ibrahim@efreshli.com>
2024-12-19 18:07:52 +0200

    add skeleton loader

--- NavigationStackCoordinator/Views/Tabs/HomeView.swift
+++ NavigationStackCoordinator/Views/Tabs/HomeView.swift
@@ -1,6 +1,9 @@
+import SkeletonUI
 import SwiftUI
 
 struct HomeView: View {
+    @State var isLoading: Bool = true
+    
     var body: some View {
         ZStack {
             VStack {
@@ -9,12 +12,20 @@ struct HomeView: View {
 
                 Text(L10n.hello)
                     .customFont(size: 20, weight: .semibold)
+                    .skeleton(with: isLoading)
+                    .frame(maxWidth: 200, maxHeight: 40)
+
                 Text(L10n.lookingGood)
                     .customFont(size: 20, weight: .semibold)
             }
             .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                isLoading = false
+            }
+        }
     }
 }
 
